e9ff8dc04e814b3511caf5f7234e80ce3651684a
SurajPrakash <prakash.champ999@gmail.com>
2023-05-11 15:22:27 +0530

    changes | final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         RWOnly name = new RWOnly();
 
+        name.setName("suraj prakash");
+
+        String collector = name.getName();
+        System.out.println(collector);
+
 
     }
 }
